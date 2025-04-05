826645d7a8e410109247a8d4751bd120acf5a0c3
TheLuis <luisdavidgilcabarcas47@gmail.com>
2025-04-05 10:18:52 -0500

    Implementación de lógica para el sistema de filtrado de productos

--- js/index.js
+++ js/index.js
@@ -2,5 +2,25 @@
 // <    >  =>
 
 window.addEventListener('DOMContentLoaded', () => {
-    console.log('Funcionando!');
+    const filters = document.querySelectorAll('li[data-filter]');
+    const cards = document.querySelectorAll('.card');
+
+    filters.forEach(filter => {
+        filter.addEventListener('click', (e) => {
+            const filterValue = e.target.getAttribute('data-filter').toLowerCase();
+
+            filters.forEach(f => f.classList.remove('active'));
+            e.target.classList.add('active');
+
+            cards.forEach(card => {
+                const category = card.getAttribute('data-category').toLowerCase();
+
+                if (filterValue === 'all' || category === filterValue) {
+                    card.classList.remove('hide');
+                } else {
+                    card.classList.add('hide');
+                }
+            });
+        });
+    });
 });
